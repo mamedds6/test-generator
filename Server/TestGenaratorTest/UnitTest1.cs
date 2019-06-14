37ec15c4967b51243e2b366ebc790eda0bd169ef
Szymani <Szymanski996@gmail.com>
2019-06-14 11:20:12 +0200

    Added Unit tests for questionDTO and answer DTO

--- Server/TestGenaratorTest/UnitTest1.cs
+++ Server/TestGenaratorTest/UnitTest1.cs
@@ -1,4 +1,8 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TSDTestGenerator.DTO;
+using TSDTestGenerator.Model;
 
 namespace Tests
 {
@@ -7,12 +11,40 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
-        public void Test1()
+        public void DTOQuestionAnswerDTO_QuestionDTO_QuestionDTO()
         {
-            Assert.Pass();
+            QuestionDto inputQuestion = new QuestionDto();
+            inputQuestion.Content = "This is a content of first qustion";
+            inputQuestion.Answers = new HashSet<AnswerDto>();
+
+            AnswerDto inputeAnswer = new AnswerDto();
+            inputeAnswer.Content = "Answer numbre one is correct";
+            inputeAnswer.IsCorrect = true;
+            inputQuestion.Answers.Append(inputeAnswer);
+
+            Question middle = inputQuestion.Question;
+            QuestionDto outputQuestion = new QuestionDto(middle);
+
+            CollectionAssert.AreEqual(inputQuestion.Answers, outputQuestion.Answers);
+            Assert.AreSame(inputQuestion.Content, outputQuestion.Content);
+        }
+
+        [Test]
+        public void DTOQuestionAnswerDTO_AnswerDTO_AnswerDTO()
+        {
+            AnswerDto inputeAnswer = new AnswerDto();
+            inputeAnswer.Content = "Answer numbre one is correct";
+            inputeAnswer.IsCorrect = true;
+
+            QuestionAnswer qa = inputeAnswer.QuestionAnswer;
+            AnswerDto outputAnswer = new AnswerDto(qa);
+
+            Assert.AreEqual(inputeAnswer.IsCorrect, outputAnswer.IsCorrect);
+            Assert.AreSame(inputeAnswer.Content, outputAnswer.Content);
         }
     }
 }
